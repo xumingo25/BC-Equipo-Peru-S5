a6ca036b0f4d19637f5be613e83d99b1e74f9e56
Pobre Domingo <domingo.saavedra@tsoftglobal.com>
2024-03-13 13:11:15 -0300

    Se agrega a l repositorio contenidos de data driven y properties manager.

--- src/test/java/S6/tests/CPs.java
+++ src/test/java/S6/tests/CPs.java
@@ -2,16 +2,21 @@ package S6.tests;
 
 import S6.pages.HomePage;
 import S6.pages.RegisterPage;
+import S6.utils.DataDriven;
 import S6.utils.FixEncoding;
+import S6.utils.PropertiesManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+
 public class CPs {
     HomePage home;
     RegisterPage register;
     WebDriver driver;
+    ArrayList<String> dataCPs; //null
 
     @BeforeAll
     public static void start(){
@@ -20,43 +25,49 @@ public class CPs {
 
     @BeforeEach
     public void preCondioniones(){
+        dataCPs = new ArrayList<String>(); //0
         driver = new ChromeDriver();
         home = new HomePage(driver);
         register = new RegisterPage(driver);
-        home.cargarSitio("https://open.spotify.com/");
+        home.cargarSitio(PropertiesManager.obtenerProperty("url"));
         home.maximizarBrowser();
     }
 
     @AfterEach
     public void posCondiciones(){
-        //home.cerrarBrowser();
+        home.cerrarBrowser();
     }
 
     @Test
     public void CP001_Creacion_Cta_Spotify(){
+        dataCPs = DataDriven.prepararData("CP001_Creacion_Cta_Spotify");
+
         home.irARegistrarse();
-        register.crearCtaSpotify("usertest0112@algo.com","123123asds","user test","10","12","2000",0,true,true);
-        Assertions.assertEquals(home.obtenerUsername(),"user test");
+        register.crearCtaSpotify(dataCPs.get(1),dataCPs.get(2),dataCPs.get(3),dataCPs.get(4),dataCPs.get(5),dataCPs.get(6),Integer.parseInt(dataCPs.get(7)), Boolean.parseBoolean(dataCPs.get(8)),Boolean.parseBoolean(dataCPs.get(9)));
+        Assertions.assertEquals(dataCPs.get(10),home.obtenerUsername());
     }
 
     @Test
     public void CP002_Error_Creacion_Cta_Spotify_Password_9Caracteres(){
+        dataCPs = DataDriven.prepararData("CP002_Error_Creacion_Cta_Spotify_Password_9Caracteres");
         home.irARegistrarse(); //12345ased
-        register.ingresarEmail("usertest01fdsfsd0@algo.com");
+        register.ingresarEmail(dataCPs.get(1));
         register.siguiente();
-        register.ingresarPassword("12345ased");
+        register.ingresarPassword(dataCPs.get(2));
         register.siguiente();
-        Assertions.assertEquals("10 caracteres\n" +
-                "Requisito no cumplido. ,",register.error9CaracteresPassword());
+        System.out.println(dataCPs.get(3));
+        System.out.println(register.error9CaracteresPassword());
+        Assertions.assertEquals(dataCPs.get(3),register.error9CaracteresPassword());
     }
 
 
     @Test
     public void CP003_Error_Creacion_Cta_mail_utilizado(){
+        dataCPs = DataDriven.prepararData("CP003_Error_Creacion_Cta_mail_utilizado");
         home.irARegistrarse(); //usertest010@algo.com
-        register.ingresarEmail("usertest010@algo.com");
+        register.ingresarEmail(dataCPs.get(1));
         register.siguiente();
-        String Esperado = FixEncoding.corregirEncoding("Esta dirección ya está vinculada a una cuenta. Para continuar, inicia sesión.");
+        String Esperado = dataCPs.get(2);
         Assertions.assertEquals(Esperado,register.errorMailIngresado());
 
     }
